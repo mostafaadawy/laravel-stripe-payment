dc6cdf6c9c572ed433c3c228922ffb381ab75697
mostafaadawy <mostafa_adawy@ymail.com>
2023-02-08 13:54:13 +0200

    stripe create checkout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,6 +15,35 @@ class ProductController extends Controller
 
     public function checkout()
     {
-
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+        $products = Product::all();
+        $lineItems = [];
+        foreach($products as $product){
+            $lineItems[]=[
+                'price_data'=> 'usd',
+                'product_data'=>[
+                    'name' => $product->name,
+                    'images' => $product->image
+                ],
+                'unit_amount'=>$product->price *100,
+            ],
+                'quantity'=>1,
+        }
+        $session = $stripe->checkout->sessions->create([
+        'line_items' => [[
+            'price_data' => [
+            'currency' => 'usd',
+            'product_data' => [
+                'name' => 'T-shirt',
+            ],
+            'unit_amount' => 2000,
+            ],
+            'quantity' => 1,
+        ]],
+        'mode' => 'payment',
+        'success_url' => 'http://localhost:4242/success',
+        'cancel_url' => 'http://localhost:4242/cancel',
+        ]);
+        return redirect($session->url);
     }
 }
