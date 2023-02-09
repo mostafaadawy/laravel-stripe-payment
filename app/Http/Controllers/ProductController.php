068fe59865e3c3f606e13ab79746f65a84c056dc
mostafaadawy <mostafa_adawy@ymail.com>
2023-02-09 19:01:07 +0200

    webhook code cpy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -87,7 +87,48 @@ class ProductController extends Controller
     }
     public function webhook(Request $request)
     {
+        // webhook.php
+        //
+        // Use this sample code to handle webhook events in your integration.
+        //
+        // 1) Paste this code into a new file (webhook.php)
+        //
+        // 2) Install dependencies
+        //   composer require stripe/stripe-php
+        //
+        // 3) Run the server on http://localhost:4242
+        //   php -S localhost:8000
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        $endpoint_secret = 'whsec_9bd02e531f9f202bd73cac39a9902725782d452f429651371f14974ed185e04e';
 
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+          $event = \Stripe\Webhook::constructEvent(
+            $payload, $sig_header, $endpoint_secret
+          );
+        } catch(\UnexpectedValueException $e) {
+          // Invalid payload
+          http_response_code(400);
+          exit();
+        } catch(\Stripe\Exception\SignatureVerificationException $e) {
+          // Invalid signature
+          http_response_code(400);
+          exit();
+        }
+
+        // Handle the event
+        switch ($event->type) {
+          case 'payment_intent.succeeded':
+            $paymentIntent = $event->data->object;
+          // ... handle other event types
+          default:
+            echo 'Received unknown event type ' . $event->type;
+        }
+
+        http_response_code(200);
     }
 
 }
